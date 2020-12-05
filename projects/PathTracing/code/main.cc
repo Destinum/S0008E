55076358693b85be18eafbed4e00a8180ee952db
Destinum <david.hoglund.96@gmail.com>
2020-12-05 13:26:47 +0100

    PathTracing Commit

--- projects/PathTracing/code/main.cc
+++ projects/PathTracing/code/main.cc
@@ -32,66 +32,64 @@ vec3 color(const ray& r, hitable *world, int depth)
     }
 }
 
-hitable *random_scene() {
-    int n = 500;
-    hitable **list = new hitable*[n+1];
-    list[0] =  new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
-    int i = 1;
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
-            float choose_mat = drand48();
-            vec3 center(a+0.9*drand48(),0.2,b+0.9*drand48()); 
-            if ((center-vec3(4,0.2,0)).length() > 0.9) { 
-                if (choose_mat < 0.8) {  // diffuse
-                    list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
-                }
-                else if (choose_mat < 0.95) { // metal
-                    list[i++] = new sphere(center, 0.2,
-                            new metal(vec3(0.5*(1 + drand48()), 0.5*(1 + drand48()), 0.5*(1 + drand48())),  0.5*drand48()));
-                }
-                else {  // glass
-                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
-                }
-            }
-        }
-    }
+hitable *random_scene(int ListLength) {
 
+    int i = 0;
+    hitable **list = new hitable*[ListLength + 1];
+
+    list[i++] =  new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
     list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
     list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
     list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+    float Size = 0.2;
+    int Boundry = sqrt(ListLength) / 2;
+    int a = - Boundry;
+    int b = a;
+
+    while (i <= ListLength) {
+
+        //float Size = 0.4 * drand48();
+        float choose_mat = drand48();
+        vec3 center(a + 0.9 * drand48(), Size, b + 0.9 * drand48()); 
+
+        if (choose_mat < 0.7) {  // diffuse
+            list[i++] = new sphere(center, Size, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+        }
+        else if (choose_mat < 0.90) { // metal
+            list[i++] = new sphere(center, Size, new metal(vec3(0.5*(1 + drand48()), 0.5*(1 + drand48()), 0.5*(1 + drand48())),  0.5*drand48()));
+        }
+        else {  // glass
+            list[i++] = new sphere(center, Size, new dielectric(1.5));
+        }
+        
+        a++;
+        if (a >= Boundry)
+        {
+            b++;
+            a = - Boundry;
+        }
+
+    }
 
     return new hitable_list(list,i);
 }
 
 
-int main() 
+int main()
 {
-    std::ofstream img ("picture.ppm");
+    //int Fisk = 5;
+    //std::cout << "Fisk: " << Fisk << std::endl;
+    //return Fisk;
 
-    int nx = 400;
-    int ny = 200;
-    int ns = 200;
+    std::ofstream img ("picture.pgm");
+
+    int nx = 100;       //400       //Image Width
+    int ny = 50;       //200       //Image Height
+    int ns = 10;       //200
     img << "P3\n" << nx << " " << ny << "\n255\n";
 
-    // vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    // vec3 horizontal(4.0, 0.0, 0.0);
-    // vec3 vertical(0.0, 2.0, 0.0);
-    // vec3 origin(0.0, 0.0, 0.0);
-
-    int ListSize = 5;
-
-    hitable *list[ListSize];
-    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
-    list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.0));
-    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
-    list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
-
-    // float R = cos(M_PI/4);
-    // list[0] = new sphere(vec3(-R, 0, -1), R, new lambertian(vec3(0, 0, 1)));
-    // list[1] = new sphere(vec3(R, 0, -1), R, new lambertian(vec3(1, 0, 0)));
-    hitable *world = new hitable_list(list, ListSize);
-    world = random_scene();
+    hitable *world = random_scene(500);
 
     vec3 lookfrom(13,2,3);
     vec3 lookat(0,0,0);
